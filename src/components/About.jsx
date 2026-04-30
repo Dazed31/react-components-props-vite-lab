3d78c8fe6de3098e4a39d261335d67d7efe8d659
Dazed31 <darrenamore13@gmail.com>
2026-04-30 15:03:57 +0300

    create React blog project with components, props, and basic structure

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+function About(props) {
+  
+  const image = props.image || "https://via.placeholder.com/215";
+
+  return (
+    <aside>
+      <img src={image} alt="blog logo" />
+      <p>{props.about}</p>
+    </aside>
+  );
+}
+
+export default About;
